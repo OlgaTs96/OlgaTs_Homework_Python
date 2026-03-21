b61f6777bec1e8a2053e687ba508461c4870ad62
OlgaTs96 <olga.tsapiro@mail.ru>
2026-03-21 19:20:41 +0300

    homework 9

--- 09_lesson/test_database.py
+++ 09_lesson/test_database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine, inspect
-import pytest
 
 db_connection_string = "postgresql://postgres:Olga1996@localhost:5433/postgres"
 db = create_engine(db_connection_string)
 
 
-@pytest
 def test_db_connection():
     inspector = inspect(db)
     names = inspector.get_table_names()
